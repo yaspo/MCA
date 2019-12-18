5f30640323fd3d9c1fa3d9dac60fc443eaff6647
VINXIS <oykxf2@gmail.com>
2019-12-18 02:42:43 -0700

    get host/port from config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
+import { Config } from "../config"
+const config = new Config;
 export default {
   serverMiddleware: ['~/api'],
   buildModules: ['@nuxt/typescript-build'],
+  server: {
+    host: config.mca.host,
+    port: config.mca.port,
+  },
 }
