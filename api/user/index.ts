75072f8dc2fdaac720f1820184db23d9a1f86890
VINXIS <oykxf2@gmail.com>
2019-12-23 05:02:13 -0700

    fix

--- api/user/index.ts
+++ api/user/index.ts
@@ -4,6 +4,10 @@ import { UserInfo } from '../../../CorsaceModels/user';
 const userRouter = new Router();
 
 userRouter.get("/", async (ctx) => {
+    if (!ctx.state.user) {
+        return ctx.body = { error: "No user found!" }
+    }
+
     const user: UserInfo = ctx.state.user.getInfo()
     ctx.body = { user }
 })
