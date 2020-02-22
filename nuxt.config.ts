5bc47ff861dde458cfe43ec98d4852fefb4cfe79
staddle <hirofushikar@gmail.com>
2020-02-22 19:15:51 +0100

    created sample site for translation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,34 @@ export default {
     host: config.mca.host,
     port: config.mca.port,
   },
+  modules: [
+    [
+      'nuxt-i18n',
+      {
+        locales: [
+          {
+            code: 'en', 
+            name: 'English',
+            file: 'en.json'
+          },
+          {
+            code: 'de', 
+            name: 'German',
+            file: 'de.json'
+          },
+          {
+            code: 'cn', 
+            name: 'Chinese',
+            file: 'cn.json'
+          },
+        ],
+        defaultLocale: 'en',
+        lazy: true,
+        langDir: '../CorsaceAssets/lang/',
+        vueI18n: {
+          fallbackLocale: 'en',
+        }
+      }
+    ]
+  ],
 }
---
+++ pages/jsonFiller.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+	<nuxt-link
+		v-for="locale in availableLocales"
+		:key="locale.code"
+		:to="switchLocalePath(locale.code)"
+		>
+		<p>{{ locale.name }}</p>
+	</nuxt-link>
+
+
+	<div v-html="$t('ayim.index.home')" />
+  </div>
+</template>
+
+<script>
+
+export default {
+	computed: {
+		availableLocales () {
+			return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
+		}
+	}
+}
+
+</script>
